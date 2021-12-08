98be97543fc95cdef3ea5bdc057323f44f584a9b
David M. Anderson <davidand36@gmail.com>
2021-12-07 21:35:42 -0800

    Webpack and TypeScript configured

---
+++ webpack.common.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Ripple Cam',
+      template: 'src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
