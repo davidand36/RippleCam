d95ced92d5b1e733737ab21a854bb3ed8afc9bcb
David M. Anderson <davidand36@gmail.com>
2021-12-13 11:40:53 -0800

    Type defs for assets for use with Typescript and Webpack

---
+++ src/assets.d.ts
@@ -0,0 +1,85 @@
+/*
+  assets.d.ts
+
+  Type declarations for non-code assets as URLs, for use with Webpack
+*/
+
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpeg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.gif' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.opus' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.ogg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.mp3' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.ogv' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.mp4' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.webm' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.woff' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.woff2' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.eot' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.ttf' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.otf' {
+  const content: string;
+  export default content;
+}
